02e7e9f9d2a6eb97e9db91f98c7d4a7d3771f7d5
orafeng <15084046@qq.com>
2023-09-08 14:53:32 +0800

    init

--- src/main/java/com/base/short2long/response/ResponseDealerImpl.java
+++ src/main/java/com/base/short2long/response/ResponseDealerImpl.java
@@ -30,8 +30,8 @@ public class ResponseDealerImpl implements ResponseDealer {
     public void handleMessage(NettyMessage nettyMessage) {
         try {
             logger.info("handle response message:");
-            if (nettyMessage.getBytesIn().length > 2) {
-                byte[] byteIdx = new byte[2];
+            if (nettyMessage.getBytesIn().length > Constants.CHANNEL_IDX_LEN) {
+                byte[] byteIdx = new byte[Constants.CHANNEL_IDX_LEN];
                 System.arraycopy(nettyMessage.getBytesIn(), 0, byteIdx, 0, Constants.CHANNEL_IDX_LEN);
                 Integer channelIdx = Integer.parseInt(LoUtils.byte2HexStr(byteIdx), 16);
                 logger.info("channel map index: {} from response message.", channelIdx);
@@ -40,10 +40,10 @@ public class ResponseDealerImpl implements ResponseDealer {
                 logger.info("current cache size: {}, {}", SysConfig.channelMap.getCache().size(), SysConfig.channelMap.getCache().stats());
                 if (!Objects.isNull(requestChannel)) {
                     Duration duration = Duration.between(requestChannel.getReqTime(), LocalDateTime.now());
-                    logger.info("channelIdx: {}, duration: {}, ctx: {}",  channelIdx, duration.toMillis(), requestChannel.getCtx());
+                    logger.info("channelIdx: {}, duration: {}, ctx: {}", channelIdx, duration.toMillis(), requestChannel.getCtx());
                     byte[] byteOut = new byte[nettyMessage.getBytesIn().length];
                     //返回
-                    System.arraycopy(nettyMessage.getBytesIn(), Constants.CHANNEL_IDX_LEN, byteOut, 0, nettyMessage.getBytesIn().length - 2);
+                    System.arraycopy(nettyMessage.getBytesIn(), Constants.CHANNEL_IDX_LEN, byteOut, 0, nettyMessage.getBytesIn().length - Constants.CHANNEL_IDX_LEN);
                     NettyServer.sendMessage(requestChannel.getCtx(), byteOut);
                 } else {
                     logger.info("not found channelIdx: {}", channelIdx);
